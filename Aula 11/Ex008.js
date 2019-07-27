ed75c497879ea6fea075270d4739f95a1280c2a8
Alisson Welder <alissonwelders@gmail.com>
2019-07-26 22:01:30 -0300

    curso javascript

---
+++ Aula 11/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){ // condição simples, pois ela nao tem senão(else)
+    console.log('Você ultrapassou a velocidade permitida')
+}
+console.log ('Dirija sempre usando o cinto de segurança')
---
+++ Aula 11/Ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Estados Unidos'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){ // condiçoes composta, com o else(senão)
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
